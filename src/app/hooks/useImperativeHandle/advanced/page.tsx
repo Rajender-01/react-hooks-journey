f14f2437b0ead6bed35b30066956c901a0688987
Rajender <ft.rajender@gmail.com>
2026-03-27 16:03:28 +0530

    Add AdvancedImperative component with useImperativeHandle for form control

---
+++ src/app/hooks/useImperativeHandle/advanced/page.tsx
@@ -0,0 +1,11 @@
+import AdvancedImperative from '@/src/hooks-learning/03-advance-hooks/useImperativeHandle/advanceForm/Parent'
+
+const page = () => {
+  return (
+    <div>
+      <AdvancedImperative/>
+    </div>
+  )
+}
+
+export default page
