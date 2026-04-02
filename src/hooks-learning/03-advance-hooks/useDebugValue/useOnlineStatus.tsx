be591a8f170f63564e21434498116c5d51e0dac6
Rajender <ft.rajender@gmail.com>
2026-04-02 12:56:05 +0530

    Refactor UseOnlineStatus hook to improve useDebugValue implementation

--- src/hooks-learning/03-advance-hooks/useDebugValue/useOnlineStatus.tsx
+++ src/hooks-learning/03-advance-hooks/useDebugValue/useOnlineStatus.tsx
@@ -2,23 +2,26 @@
 import { useDebugValue, useEffect, useState } from "react";
 
 const UseOnlineStatus = () => {
-
   const [isOnline, setIsOnline] = useState<boolean | null>(null);
- useEffect(() => {
-  const updateStatus = () => setIsOnline(navigator.onLine);
-  updateStatus(); 
-  const goOnline = () => setIsOnline(true);
-  const goOffline = () => setIsOnline(false);
+  useEffect(() => {
+    const updateStatus = () => setIsOnline(navigator.onLine);
+    updateStatus();
+    const goOnline = () => setIsOnline(true);
+    const goOffline = () => setIsOnline(false);
 
-  window.addEventListener("online", goOnline);
-  window.addEventListener("offline", goOffline);
+    window.addEventListener("online", goOnline);
+    window.addEventListener("offline", goOffline);
 
-  return () => {
-    window.removeEventListener("online", goOnline);
-    window.removeEventListener("offline", goOffline);
-  };
-}, []);
- useDebugValue(isOnline ? "Online 🟢" : "Offline 🔴");
+    return () => {
+      window.removeEventListener("online", goOnline);
+      window.removeEventListener("offline", goOffline);
+    };
+  }, []);
+  // basic usecase - run without open devtool
+  // useDebugValue(isOnline ? "Online 🟢" : "Offline 🔴");
+
+  // Intermediate usecase - run only after the devtool open
+  useDebugValue(isOnline, (status) => (status ? "Online 🟢" : "Offline 🔴"));
   return isOnline;
 };
 
